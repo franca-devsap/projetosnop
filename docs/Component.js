137cc01ed3c5a999ded6e31b15e6b90830ceaf6e
franca-devsap <valdemirdefranca@mac.com>
2026-06-24 13:03:39 -0300

    docs

---
+++ docs/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/UIComponent"],function(e){"use strict";return e.extend("zsd.forecast.unrestricted.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
